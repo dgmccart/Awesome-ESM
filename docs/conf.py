cca25f36dc4be984d45ca99f15f820565beec29d
dgmccart <92180364+dgmccart@users.noreply.github.com>
2026-06-25 17:25:29 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ intersphinx_mapping = {
 }
 
 # Paths and file configuration
-templates_path = ["_templates"] 
+# templates_path = ["_templates"] 
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 # External TOC
